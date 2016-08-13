b7346ed6eacef380be44ce527f242e16f3ba3de1
RafayelGardishyan <rgardishyan@gmail.com>
2016-08-13 19:33:35 +0200

    Update main.rb

--- New Project-20160813/main.rb
+++ New Project-20160813/main.rb
@@ -1,22 +1,22 @@
-print "Your first name please"
+print "Your first name please\n"
 name = gets.chomp
 name.capitalize!
 
-print "Your last name please"
+print "Your last name please\n"
 last = gets.chomp
 last.capitalize!
 
-print "Your birth date please"
+print "Your birth date please (dd/mm/yyyy)\n"
 birth = gets.chomp
 birth.capitalize!
 
-print "Your first name is #{name}, your last name is #{last} and you were born on #{birth} is this good? type OK"
+print "Your first name is #{name}, your last name is #{last} and you were born on #{birth} is this good? type OK\n"
 ok = gets.chomp.downcase
 
 if ok.include? "ok"
-    print "You are registred!"
+    print "You are registred!\n"
 else
-    print "Try again!"
+    print "Try again!\n"
 end
 
-puts"Thanks for using our service"
+puts"Thanks for using our service\n"
