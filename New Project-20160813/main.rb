44b61632824dfdb95445f2fbce8b0177f02ef9e0
RafayelGardishyan <rgardishyan@gmail.com>
2016-08-13 19:26:44 +0200

    Created by Coding Ground Client

---
+++ New Project-20160813/main.rb
@@ -0,0 +1,22 @@
+print "Your first name please"
+name = gets.chomp
+name.capitalize!
+
+print "Your last name please"
+last = gets.chomp
+last.capitalize!
+
+print "Your birth date please"
+birth = gets.chomp
+birth.capitalize!
+
+print "Your first name is #{name}, your last name is #{last} and you were born on #{birth} is this good? type OK"
+ok = gets.chomp.downcase
+
+if ok.include? "ok"
+    print "You are registred!"
+else
+    print "Try again!"
+end
+
+puts"Thanks for using our service"
